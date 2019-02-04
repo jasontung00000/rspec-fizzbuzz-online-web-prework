3fc6d32e80905c3780d5ebe2a1607e74b08f0b32
Jason Tung <jasontung123@yahoo.com>
2019-02-04 19:35:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(Integer)
-  if Integer % 3 == 0
+def fizzbuzz(Int)
+  if Int % 3 == 0
     "Fizz"
 end
 end
